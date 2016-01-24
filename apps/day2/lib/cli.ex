8ca0ad180cca4871befbcf7d9b8cca3d43d04af0
barruumrex <barruumrex@gmail.com>
2016-01-23 20:17:13 -0500

    Finished day2

--- apps/day2/lib/cli.ex
+++ apps/day2/lib/cli.ex
@@ -12,8 +12,28 @@ defmodule Day2.CLI do
   defp process({:ok, filename}) do
     filename
     |> File.stream!
-    |> Enum.reduce(0, fn(line, acc) -> acc + (line |> String.strip |> Day2.get_paper_amount) end)
-    |> IO.puts
+    |> Enum.reduce({0, 0}, &process_line/2)
+    |> output_response
+  end
+
+  defp process_line(text, acc) do
+    text
+    |> String.strip
+    |> get_paper_and_ribbon
+    |> add_to(acc)
+  end
+
+  defp get_paper_and_ribbon(text) do
+    {Day2.get_paper_amount(text), Day2.get_ribbon_amount(text)}
+  end
+
+  defp add_to({new_paper, new_ribbon}, {total_paper, total_ribbon}) do
+    {new_paper + total_paper, new_ribbon + total_ribbon}
+  end
+
+  defp output_response({paper, ribbon}) do
+    IO.puts "The elves need #{paper} square feet of wrapping paper"
+    IO.puts "The elves need #{ribbon} feet of ribbon"
   end
 
   defp parse_args(args) do
--- apps/day2/lib/day2.ex
+++ apps/day2/lib/day2.ex
@@ -31,8 +31,42 @@ defmodule Day2 do
   end
 
   @spec get_sides({integer, integer, integer}) :: {integer, integer, integer}
-  defp get_sides({l, w, h}), do: {l*w, w*h, h*l}
+  defp get_sides({l, w, h}), do: {l * w, w * h, h * l}
 
   @spec get_total({integer, integer, integer}) :: integer
-  defp get_total({a, b, c}), do: 2*a + 2*b + 2*c + Enum.min([a, b, c])
+  defp get_total({a, b, c}), do: 2 * a + 2 * b + 2 * c + Enum.min([a, b, c])
+
+
+  @doc """
+    Takes a string representation of a box and returns the amount of necessary ribbon
+
+    #Examples
+
+        iex> Day2.get_ribbon_amount("2x3x4")
+        34
+
+        iex> Day2.get_ribbon_amount("1x1x10")
+        14
+  """
+  @spec get_ribbon_amount(String.t) :: integer
+  def get_ribbon_amount(text) do
+    text
+    |> get_dimensions
+    |> calculate_lengths
+  end
+
+  @spec calculate_lengths({integer, integer, integer}) :: integer
+  defp calculate_lengths(dimensions) do
+    calc_wrap_ribbon(dimensions) + calc_bow(dimensions)
+  end
+
+  @spec calc_wrap_ribbon({integer, integer, integer}) :: integer
+  defp calc_wrap_ribbon({l, w, h}) do
+    [l, w, h]
+    |> List.delete(Enum.max([l, w, h]))
+    |> Enum.reduce(0, fn(x, acc) -> acc + (2 * x) end)
+  end
+
+  @spec calc_bow({integer, integer, integer}) :: integer
+  defp calc_bow({l, w, h}), do: l * w * h
 end
