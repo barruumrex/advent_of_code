0072963f95a48b9f4bd9f4e0392dc82afd9c8f78
barruumrex <barruumrex@gmail.com>
2016-01-31 12:01:57 -0500

    Convert to numbers in prep for part 2

--- apps/day6/lib/day6.ex
+++ apps/day6/lib/day6.ex
@@ -22,8 +22,8 @@ defmodule Day6 do
   def perform_instructions(instructions) do
     instructions
     |> Enum.reduce(%{}, &perform_instruction/2)
-    |> Enum.reduce(0, fn {_key, :on}, acc -> acc + 1
-                         {_key, :off}, acc -> acc end)
+    |> Enum.reduce(0, fn {_key, 1}, acc -> acc + 1
+                         {_key, 0}, acc -> acc end)
   end
 
   @doc """
@@ -32,10 +32,10 @@ defmodule Day6 do
   #Examples
 
       iex> Day6.perform_instruction("turn on 0,0 through 1,1", %{})
-      %{{0, 0} => :on, {0, 1} => :on, {1, 0} => :on, {1, 1} => :on}
+      %{{0, 0} => 1, {0, 1} => 1, {1, 0} => 1, {1, 1} => 1}
 
-      iex> Day6.perform_instruction("toggle 0,0 through 1,1", %{{0, 0} => :on})
-      %{{0, 0} => :off, {0, 1} => :on, {1, 0} => :on, {1, 1} => :on}
+      iex> Day6.perform_instruction("toggle 0,0 through 1,1", %{{0, 0} => 1})
+      %{{0, 0} => 0, {0, 1} => 1, {1, 0} => 1, {1, 1} => 1}
   """
   @spec perform_instruction(String.t, map) :: map
   def perform_instruction(instruction, lights) do
@@ -45,7 +45,7 @@ defmodule Day6 do
     Map.merge(lights, Map.new(changes))
   end
 
-  @spec parse_instruction(String.t) :: {(coordinate, Map -> {coordinate, :on | :off}), list(coordinate)}
+  @spec parse_instruction(String.t) :: {(coordinate, Map -> {coordinate, 1 | 0}), list(coordinate)}
   defp parse_instruction("turn on " <> tail), do: {&on/2, get_coordinates(tail)}
   defp parse_instruction("turn off " <> tail), do: {&off/2, get_coordinates(tail)}
   defp parse_instruction("toggle " <> tail), do: {&toggle/2, get_coordinates(tail)}
@@ -65,21 +65,21 @@ defmodule Day6 do
     |> List.to_tuple
   end
 
-  @spec toggle(coordinate, map) :: {coordinate, :on | :off}
+  @spec toggle(coordinate, map) :: {coordinate, 1 | 0}
   defp toggle(key, lights) do
     value = lights
-      |> Map.get(key, :off)
+      |> Map.get(key, 0)
       |> do_toggle
   {key, value}
   end
 
-  @spec do_toggle(:on | :off) :: :on | :off
-  defp do_toggle(:on), do: :off
-  defp do_toggle(:off), do: :on
+  @spec do_toggle(1 | 0) :: 1 | 0
+  defp do_toggle(1), do: 0
+  defp do_toggle(0), do: 1
 
-  @spec on(coordinate, map) :: tuple
-  defp on(key, _lights), do: {key, :on}
+  @spec on(coordinate, map) :: {coordinate, 1}
+  defp on(key, _lights), do: {key, 1}
 
-  @spec off(coordinate, map) :: tuple
-  defp off(key, _lights), do: {key, :off}
+  @spec off(coordinate, map) :: {coordinate, 0}
+  defp off(key, _lights), do: {key, 0}
 end
