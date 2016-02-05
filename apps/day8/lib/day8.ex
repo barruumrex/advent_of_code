aec3583dab70d6bea63d228e3303f524b36ccf38
barruumrex <barruumrex@gmail.com>
2016-02-04 21:44:07 -0500

    Finished day8

--- apps/day8/lib/day8.ex
+++ apps/day8/lib/day8.ex
@@ -30,6 +30,48 @@ defmodule Day8 do
     memory_length = line
       |> Code.eval_string()
       |> elem(0)
-      |> String.length
+      |> String.length()
   end
+
+  @doc """
+  Escaped length minus memory length of a string
+
+  ## Examples
+
+      iex> Day8.escape_diff(~S(""))
+      4
+
+      iex> Day8.escape_diff(~S("abc"))
+      4
+
+      iex> Day8.escape_diff(~S("aaa\\"aaa"))
+      6
+
+      iex> Day8.escape_diff(~S("\\x27"))
+      5
+  """
+  @spec escape_diff(String.t) :: non_neg_integer
+  def escape_diff(line) do
+    escape_length(line) - String.length(line)
+  end
+
+  @spec escape_length(String.t) :: non_neg_integer
+  def escape_length(line) do
+    line
+    |> escape_string()
+    |> String.length()
+  end
+
+  @spec escape_string(String.t) :: String.t
+  defp escape_string(line) do
+    line
+    |> String.codepoints()
+    |> Enum.reduce(~S(""), fn(x,acc) -> acc <> escape_char(x) end)
+  end
+
+  @spec escape_char(String.t) :: String.t
+  defp escape_char("\""), do: ~S(\")
+  defp escape_char("\\"), do: ~S(\\)
+  defp escape_char(x), do: x
+
 end
--- apps/day8/lib/runner.exs
+++ apps/day8/lib/runner.exs
@@ -4,3 +4,9 @@
   |> Enum.reduce(0, fn(line, acc) -> acc + Day8.memory_diff(line) end)
   |> IO.inspect()
 
+"lib/day8.txt"
+  |> File.read!()
+  |> String.split("\n", trim: true)
+  |> Enum.reduce(0, fn(line, acc) -> acc + Day8.escape_diff(line) end)
+  |> IO.inspect()
+
