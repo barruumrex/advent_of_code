8f606934df892f44c9b2a4a5d85dbc706acfcf52
barruumrex <barruumrex@gmail.com>
2016-02-14 12:54:28 -0500

    Incrementing works

--- apps/day11/lib/day11.ex
+++ apps/day11/lib/day11.ex
@@ -2,4 +2,23 @@ defmodule Day11 do
   @moduledoc """
   Helper functions for Day11 of Advent of Code
   """
+
+  @doc """
+  Increment 8 char string to the next value
+  """
+  @spec increment(String.t) :: String.t
+  def increment(current) when byte_size(current) != 8, do: raise ArgumentError
+  def increment(current) do
+    current
+    |> String.to_char_list()
+    |> Enum.reverse()
+    |> do_increment()
+    |> Enum.reverse()
+    |> to_string()
+  end
+
+  @spec do_increment(char_list) :: char_list
+  defp do_increment([]), do: []
+  defp do_increment([?z | rest]), do: [?a | do_increment(rest)]
+  defp do_increment([char | rest]), do: [char + 1 | rest]
 end
--- apps/day11/test/day11_test.exs
+++ apps/day11/test/day11_test.exs
@@ -2,4 +2,19 @@ defmodule Day11Test do
   use ExUnit.Case
   doctest Day11
 
+  test "Increment fails if not exactly 8 characters" do
+    assert_raise(ArgumentError, fn -> Day11.increment("a") end)
+  end
+
+  test "Increment increments the last letter" do
+    assert Day11.increment("aaaaaaaa") == "aaaaaaab"
+  end
+
+  test "Incrementing \"z\" rolls over to next letter" do
+    assert Day11.increment("aaaaaaaz") == "aaaaaaba"
+  end
+
+  test "Incrementing all z's rolls over to all a's" do
+    assert Day11.increment("zzzzzzzz") == "aaaaaaaa"
+  end
 end
