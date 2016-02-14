e8dbad6a595117e0bb68b1cf92ed2f595888acec
barruumrex <barruumrex@gmail.com>
2016-02-14 13:09:55 -0500

    Use charlist for internal functions instead of strings

--- apps/day11/lib/day11.ex
+++ apps/day11/lib/day11.ex
@@ -4,21 +4,28 @@ defmodule Day11 do
   """
 
   @doc """
-  Increment 8 char string to the next value
+  Increment char string to the next value
+
+  ## Examples
+
+      iex> Day11.increment('xy')
+      'xz'
+
+      iex> Day11.increment('xz')
+      'ya'
   """
-  @spec increment(String.t) :: String.t
-  def increment(current) when byte_size(current) != 8, do: raise ArgumentError
+  @spec increment(char_list) :: char_list
   def increment(current) do
     current
-    |> String.to_char_list()
     |> Enum.reverse()
     |> do_increment()
     |> Enum.reverse()
-    |> to_string()
   end
 
   @spec do_increment(char_list) :: char_list
   defp do_increment([]), do: []
   defp do_increment([?z | rest]), do: [?a | do_increment(rest)]
   defp do_increment([char | rest]), do: [char + 1 | rest]
+
+
 end
--- apps/day11/test/day11_test.exs
+++ apps/day11/test/day11_test.exs
@@ -2,19 +2,15 @@ defmodule Day11Test do
   use ExUnit.Case
   doctest Day11
 
-  test "Increment fails if not exactly 8 characters" do
-    assert_raise(ArgumentError, fn -> Day11.increment("a") end)
-  end
-
   test "Increment increments the last letter" do
-    assert Day11.increment("aaaaaaaa") == "aaaaaaab"
+    assert Day11.increment('aaaaaaaa') == 'aaaaaaab'
   end
 
-  test "Incrementing \"z\" rolls over to next letter" do
-    assert Day11.increment("aaaaaaaz") == "aaaaaaba"
+  test "Incrementing 'z' rolls over to next letter" do
+    assert Day11.increment('aaaaaaaz') == 'aaaaaaba'
   end
 
   test "Incrementing all z's rolls over to all a's" do
-    assert Day11.increment("zzzzzzzz") == "aaaaaaaa"
+    assert Day11.increment('zzzzzzzz') == 'aaaaaaaa'
   end
 end
