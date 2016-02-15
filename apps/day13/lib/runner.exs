92db2f87a7a11bb070297723b3c44d4c240b59d5
barruumrex <barruumrex@gmail.com>
2016-02-14 23:13:32 -0500

    Day 13 complete

--- apps/day13/lib/runner.exs
+++ apps/day13/lib/runner.exs
@@ -1,5 +1,3 @@
-require IEx
-
 relationships = "lib/input.txt"
   |> File.read!()
   |> Day13.parse()
@@ -10,3 +8,24 @@ seatings = relationships
 result = Day13.find_max_happiness(relationships, seatings)
 IO.puts "Maximum happiness is #{result}"
 
+
+with_me = """
+Me would gain 0 happiness units by sitting next to Alice.
+Me would gain 0 happiness units by sitting next to Bob.
+Me would gain 0 happiness units by sitting next to Carol.
+Me would gain 0 happiness units by sitting next to David.
+Me would gain 0 happiness units by sitting next to Eric.
+Me would gain 0 happiness units by sitting next to Frank.
+Me would gain 0 happiness units by sitting next to George.
+Me would gain 0 happiness units by sitting next to Mallory.
+"""
+relationships = "lib/input.txt"
+  |> File.read!()
+  |> (fn(x) -> x <> with_me end).()
+  |> Day13.parse()
+
+seatings = relationships
+  |> Day13.get_potential_seatings()
+
+result = Day13.find_max_happiness(relationships, seatings)
+IO.puts "Maximum happiness including me is #{result}"
